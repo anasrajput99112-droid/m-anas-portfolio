cfde4f5762c4f47246d923807c2df0b7b0b0374f
Anas <anasrajput99112@gmail.com>
2026-01-29 18:05:30 +0500

    new changes

--- js files/script.js
+++ js files/script.js
@@ -1,6 +1,14 @@
-let menubtn = document.getElementById('menu-bar');
-let navlink = document.getElementById('nav-links');
+let menubtn = document.getElementById("menu-bar");
+let navlink = document.getElementById("nav-links");
 
-menubtn.addEventListener('click', () => {
-    navlink.classList.toggle('active');
-})
+menubtn.addEventListener("click", () => {
+  navlink.style.display = "flex";
+  navlink.style.top = "0px";
+  navlink.style.transition = "all .25 ease";
+});
+
+navlink.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navlink.remove("navlink");
+  });
+});
