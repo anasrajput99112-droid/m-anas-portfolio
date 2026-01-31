af2265d5cbda8fa9a9c4db80ce90183fd4df526d
Anas <anasrajput99112@gmail.com>
2026-01-31 13:38:49 +0500

    some changes in menubar

--- js files/script.js
+++ js files/script.js
@@ -1,14 +1,19 @@
 let menubtn = document.getElementById("menu-bar");
 let navlink = document.getElementById("nav-links");
 
+// Open menu when hamburger is clicked
 menubtn.addEventListener("click", () => {
   navlink.style.display = "flex";
   navlink.style.top = "0px";
-  navlink.style.transition = "all .25 ease";
+  navlink.style.transition = "all .25s ease";
+  menubtn.classList.add("active");
 });
 
+// When a menu link is clicked: close the menu (don't remove it from DOM)
 navlink.querySelectorAll("a").forEach((link) => {
   link.addEventListener("click", () => {
-    navlink.remove("navlink");
+    navlink.style.display = "none";
+    navlink.style.top = "-10px";
+    menubtn.classList.remove("active");
   });
 });
